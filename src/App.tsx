6a77803542cec03397007a983187ee2afefbca18
Ryota <ryoenoki2013@gmail.com>
2022-10-06 17:09:44 +0900

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import {Navbar, Hero, Stats, Business, Billing, CardDeal, Testimonials, Clients,
 Footer} from './components'
 
 const App = () => (
-  <div className='bg-primary w-full overflow-hidden text-white'>
+  <div className='bg-primary w-full overflow-hidden'>
     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
         <Navbar />
